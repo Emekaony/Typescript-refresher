858bd5db91e368e6f2cca4a44589358e744a94b3
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-09-27 14:19:25 -0700

    Done with ts course, time to go further on this path by building projects.

---
+++ src/decorators.ts
@@ -0,0 +1,7 @@
+/*
+usually used by developers working on large libraries & frameworks.
+Not necessary at all for small projects.
+
+they are another way of meta programming, similar to macros in Rust.
+Python also has decorators.
+*/
